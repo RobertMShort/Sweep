1aa0160eb52b971398c483bcf504683cffdb9f7c
rshort2981 <134110968+rshort2981@users.noreply.github.com>
2023-05-19 19:27:36 -0400

    Create sweep.sh

---
+++ sweep.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+source banners.sh
+source func.sh
+
+sweep () {
+	if ! type nmap > /dev/null; then
+		echo "Nmap not installed...\n"
+		echo "Install Nmap and come back!\n"
+		echo "Try: sudo apt-get nmap"
+	fi
+
+	if [ "$1" == "-h" ]; then
+		less IPsweep/help.txt
+	elif [ "$1" == "" ] || [ "$1" == "-s" ] || [ "$1" == "-a" ]; then
+		logo
+		make_dir
+    	ip=$(ifconfig | awk '/broadcast/ {print $2}' | cut -c 1-9)
+    	scanning_network
+    	ip_for_loop "$ip"
+    	sleep 1
+    	active_hosts
+    	cat IPsweep/ips.txt
+    	printf "\n-Saved to IPsweep/ip.txt\n"
+    	sleep 1
+    	line
+		  scanning_ports
+
+		  select_scan "$1"
+
+		  active_ports
+
+		  cat IPsweep/ports.txt
+
+   		printf "\n-Saved to IPsweep/ports.txt\n"
+
+    	echo " " >> IPsweep/ips.txt
+    		date >> IPsweep/ips.txt
+
+		  line
+		  options
+
+		  read run
+		  target_scan "$run"
+   	 elif [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+   	 	logo
+   	 	make_dir
+		  scanning_network
+		  ip_for_loop "$1"
+		  sleep 1
+    	active_hosts
+    	cat IPsweep/ips.txt
+    	printf "\n-Saved to IPsweep/ip.txt\n"
+    	sleep 1
+		  line
+		  scanning_ports
+
+		  select_scan "$2"
+
+		  active_ports
+
+		  cat IPsweep/ports.txt
+
+   		printf "\n-Saved to IPsweep/ports.txt\n"
+
+    	echo " " >> IPsweep/ips.txt
+    		date >> IPsweep/ips.txt
+
+		  line
+		  options
+
+		  read run
+
+	    target_scan "$run"
+	else
+		echo "Not a valid IP"
+	fi
+}
+
+sweep "$1" "$2"
