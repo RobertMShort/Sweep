22fe720867672aa4f332e91326bee9077bae9f6d
rshort2981 <134110968+rshort2981@users.noreply.github.com>
2023-05-21 17:59:59 -0400

    Update sweep.sh

--- sweep.sh
+++ sweep.sh
@@ -11,7 +11,7 @@ sweep () {
 	fi
 
 	if [ "$1" == "-h" ]; then
-		less ~/Documents/Sweep/help.txt
+		less help.txt
 	elif [ "$1" == "" ] || [ "$1" == "-s" ] || [ "$1" == "-a" ]; then
 		logo
 		detect_OS
