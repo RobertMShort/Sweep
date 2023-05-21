5b83010e0211273d325fe027aa52d740c31fe836
rshort2981 <134110968+rshort2981@users.noreply.github.com>
2023-05-21 16:38:51 -0400

    Update func.sh

--- func.sh
+++ func.sh
@@ -7,26 +7,26 @@ exit_sweep () {
 }
 
 make_dir () {
-    if [ ! -d ~/Sweep ]
+    if [ ! -d ~/Sweep_Docs ]
      	then
-    	mkdir ~/Sweep
+    	mkdir ~/Sweep_Docs
     fi
 }
 
 ip_for_loop () {
     for h in `seq 1 254`; do
          	ping -c 1 $1.$h | grep "64 bytes" | cut -d " " -f 4 | tr -d ":"&
-    	done > ~/Sweep/ips.txt
+    	done > ~/Sweep_Docs/ips.txt
 }
 
 select_scan () {
     if [ "$1" == "" ]; then
-   	  sudo nmap -iL ~/Sweep/ips.txt > ~/Sweep/ports.txt
-	  elif [ "$1" == "-s" ]; then
-   	  sudo nmap -sV -v -iL ~/Sweep/ips.txt > ~/Sweep/ports.txt
-	  elif [ "$1" == "-a" ]; then
-   	  sudo nmap -A -v -iL ~/Sweep/ips.txt > ~/Sweep/ports.txt
-	  fi
+   	  sudo nmap -iL ~/Sweep_Docs/ips.txt > ~/Sweep_Docs/ports.txt
+    elif [ "$1" == "-s" ]; then
+   	  sudo nmap -sV -v -iL ~/Sweep_Docs/ips.txt > ~/Sweep_Docs/ports.txt
+    elif [ "$1" == "-a" ]; then
+   	  sudo nmap -A -v -iL ~/Sweep_Docs/ips.txt > ~/Sweep_Docs/ports.txt
+    fi
 }
 
 target_scan () {
@@ -34,24 +34,24 @@ target_scan () {
    	  printf "\nEnter Target IP:\n"
   	  read target
     	scanning_services
-    	sudo nmap -sV -v "$target" > ~/Sweep/services.txt
+    	sudo nmap -sV -v "$target" > ~/Sweep_Docs/services.txt
     	service_info
-    	cat ~/Sweep/services.txt
-    	printf "\n-Saved to ~/Sweep/services.txt\n"
+    	cat ~/Sweep_Docs/services.txt
+    	printf "\n-Saved to ~/Sweep_Docs/services.txt\n"
     	line
-	elif [ "$1" == "a" ]; then
+    elif [ "$1" == "a" ]; then
     	printf "\nEnter Target IP:\n"
     	read target
     	agr_scan
-    	sudo nmap -A -v "$target" > ~/Sweep/aggressive.txt
+    	sudo nmap -A -v "$target" > ~/Sweep_Docs/aggressive.txt
     	aggressive_info
   		cat ~/Sweep/aggressive.txt
-    	printf "\n-Saved to ~/Sweep/services.txt\n"
-	elif [ "$1" == "q" ]; then
+    	printf "\n-Saved to ~/Sweep_Docs/services.txt\n"
+    elif [ "$1" == "q" ]; then
     	exit_sweep
-  else
+    else
   		echo "Not a valid selection"
-	fi
+    fi
 
 }
 
