d4261e3cbb55487c008259a6ccd1b99c74c47682
rshort2981 <134110968+rshort2981@users.noreply.github.com>
2023-05-21 17:10:56 -0400

    Update func.sh

--- func.sh
+++ func.sh
@@ -7,25 +7,25 @@ exit_sweep () {
 }
 
 make_dir () {
-    if [ ! -d ~/Sweep_Docs ]
+    if [ ! -d Sweep_Docs ]
      	then
-    	mkdir ~/Sweep_Docs
+    	mkdir Sweep_Docs
     fi
 }
 
 ip_for_loop () {
     for h in `seq 1 254`; do
          	ping -c 1 $1.$h | grep "64 bytes" | cut -d " " -f 4 | tr -d ":"&
-    	done > ~/Sweep_Docs/ips.txt
+    	done > Sweep_Docs/ips.txt
 }
 
 select_scan () {
     if [ "$1" == "" ]; then
-   	  sudo nmap -iL ~/Sweep_Docs/ips.txt > ~/Sweep_Docs/ports.txt
+   	  sudo nmap -iL Sweep_Docs/ips.txt > Sweep_Docs/ports.txt
     elif [ "$1" == "-s" ]; then
-   	  sudo nmap -sV -v -iL ~/Sweep_Docs/ips.txt > ~/Sweep_Docs/ports.txt
+   	  sudo nmap -sV -v -iL Sweep_Docs/ips.txt > Sweep_Docs/ports.txt
     elif [ "$1" == "-a" ]; then
-   	  sudo nmap -A -v -iL ~/Sweep_Docs/ips.txt > ~/Sweep_Docs/ports.txt
+   	  sudo nmap -A -v -iL Sweep_Docs/ips.txt > Sweep_Docs/ports.txt
     fi
 }
 
@@ -34,10 +34,10 @@ target_scan () {
    	  printf "\nEnter Target IP:\n"
   	  read target
     	scanning_services
-    	sudo nmap -sV -v "$target" > ~/Sweep_Docs/services.txt
+    	sudo nmap -sV -v "$target" > Sweep_Docs/services.txt
     	service_info
-    	cat ~/Sweep_Docs/services.txt
-    	printf "\n-Saved to ~/Sweep_Docs/services.txt\n"
+    	cat Sweep_Docs/services.txt
+    	printf "\n-Saved to Sweep_Docs/services.txt\n"
     	line
     elif [ "$1" == "a" ]; then
     	printf "\nEnter Target IP:\n"
@@ -45,8 +45,8 @@ target_scan () {
     	agr_scan
     	sudo nmap -A -v "$target" > ~/Sweep_Docs/aggressive.txt
     	aggressive_info
-  		cat ~/Sweep/aggressive.txt
-    	printf "\n-Saved to ~/Sweep_Docs/services.txt\n"
+  		cat Sweep_Docs/aggressive.txt
+    	printf "\n-Saved to Sweep_Docs/services.txt\n"
     elif [ "$1" == "q" ]; then
     	exit_sweep
     else
